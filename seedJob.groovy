2d62d971d6fccce0ec05deae300189a9dea810c3
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-08-08 03:10:57 +0500

    Create seedJob.groovy

---
+++ seedJob.groovy
@@ -0,0 +1,12 @@
+pipelineJob('Maven-DSL-job') {
+    definition {
+    triggers {
+        githubPush()
+    }      
+        cpsScm {
+            scm {
+                git('https://github.com/sikandarqaisar/mavenApp.git')
+            }
+        }
+    }
+}
